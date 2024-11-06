b97dc3054143a38a5c862ca754763310022d802b
mohamed manseur <mohamedmanseur@MacBook-Pro-de-mohamed.local>
2024-11-06 10:50:07 +0100

    fix(upload): upoad doesn't work

---
+++ src/Entity/MediaObject.php
@@ -0,0 +1,191 @@
+<?php  /*
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\GetCollection;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Vich\UploaderBundle\Entity\File as EmbeddedFile;
+
+
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+
+use ApiPlatform\OpenApi\Model;
+use App\State\SaveMediaObject;
+
+
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+#[ORM\Entity]
+#[Vich\Uploadable]
+#[ApiResource(
+    normalizationContext: ['groups' => ['media_object:read']],
+    types: ['https://schema.org/MediaObject'],
+    outputFormats: ['jsonld' => ['application/ld+json']],
+    inputFormats:['multipart'=>['multipart/form-data']],
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            inputFormats: ['multipart' => ['multipart/form-data']],
+            openapi: new Model\Operation(
+                requestBody: new Model\RequestBody(
+                    content: new \ArrayObject([
+                        'multipart/form-data' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'file' => [
+                                        'type' => 'string',
+                                        'format' => 'binary'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ])
+                )
+            )
+        )
+    ]
+)]
+#[ORM\HasLifecycleCallbacks]
+class MediaObject
+{
+    #[ORM\Id]
+    #[ORM\Column]
+    #[ORM\GeneratedValue]
+    private ?int $id = null;
+
+    // ... other fields
+
+    // NOTE: This is not a mapped field of entity metadata, just a simple property.
+    #[Vich\UploadableField(mapping: 'products', fileNameProperty: 'image.name', size: 'image.size')]
+    private ?File $imageFile = null;
+
+    #[ORM\Embedded(class: 'Vich\UploaderBundle\Entity\File')]
+    private ?EmbeddedFile $image = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\PreUpdate]
+    public function preUpdate(){
+        $this->updatedAt = new \DateTimeImmutable();
+    }
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+   /* public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?EmbeddedFile
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage(EmbeddedFile $image): void
+    {
+        $this->image = $image;
+    }
+
+    public function getImage(): ?EmbeddedFile
+    {
+        return $this->image;
+    }
+}*/
+
+// api/src/Entity/MediaObject.php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\OpenApi\Model;
+use App\State\SaveMediaObject;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+#[Vich\Uploadable]
+#[ORM\Entity]
+#[ApiResource(
+    normalizationContext: ['groups' => ['media_object:read']],
+    denormalizationContext:['groups' => ['media_object:write']],
+    types: ['https://schema.org/MediaObject'],
+    outputFormats: ['jsonld' => ['application/ld+json']],
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            inputFormats: ['multipart' => ['multipart/form-data']],
+            openapi: new Model\Operation(
+                requestBody: new Model\RequestBody(
+                    content: new \ArrayObject([
+                        'multipart/form-data' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'file' => [
+                                        'type' => 'string',
+                                        'format' => 'binary'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ])
+                )
+            )
+        )
+    ]
+)]
+class MediaObject
+{
+    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
+    #[Groups(['media_object:read','media_object:write' ])]
+    private ?int $id = null;
+
+    #[ApiProperty(types: ['https://schema.org/contentUrl'], writable: false)]
+    #[Groups(['media_object:read','media_object:write' ])]
+    public ?string $contentUrl = null;
+
+    #[Vich\UploadableField(mapping: 'media_object', fileNameProperty: 'filePath')]
+    #[Assert\NotNull]
+    #[Groups(['media_object:read','media_object:write' ])]
+    public ?File $file = null;
+
+    #[ApiProperty(writable: false)]
+    #[ORM\Column(nullable: true)]
+    #[Groups(['media_object:read','media_object:write' ])]
+    public ?string $filePath = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
